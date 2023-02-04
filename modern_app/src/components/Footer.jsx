f4795403c4c2bad7620f28012a2030df695cc8b5
Bryan <bryanlon@yahoo.com>
2023-02-04 17:15:56 +0800

    Update footer

--- modern_app/src/components/Footer.jsx
+++ modern_app/src/components/Footer.jsx
@@ -32,9 +32,9 @@ const Footer = () => (
         </div>
       </div>
 
-      <div className="w-full flex justify-between item-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3f3r45]">
+      <div className="w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3F3R45]">
         <p className="font-poppins font-normal text-center text-[18px] leading-[27px] text-white">
-          2023 HooBank. All Rights Reserved.
+          Copyright Ⓒ 2023 HooBank. All Rights Reserved.
         </p>
         <div className="flex flex-row md:mt-0 mt-6">
           {socialMedia.map((social, index) => (
@@ -42,7 +42,8 @@ const Footer = () => (
               key={social.id}
               src={social.icon}
               alt={social.id}
-              className={`w-[21px] h-[21px] object-contain cursor-pointer ${index !== socialMedia.length - 1 ? 'mr-6' : 'mr-0'}`}
+              className={`w-[21px] h-[21px] object-contain cursor-pointer ${index !== socialMedia.length - 1 ? "mr-6" : "mr-0"}`}
+              onClick={() => window.open(social.link)}
             />
           ))}
         </div>
